8d3681385246b39b52105ca61ef29fb6e07b11cd
BOEBELIX <sboebel@t-online.de>
2019-05-18 21:43:18 +0200

    + Tabellenbedingung: Alles Spalten müssen gleich lang sein UND

--- src/de/boebelix/Kontingenz/ContingencyTableTest.java
+++ src/de/boebelix/Kontingenz/ContingencyTableTest.java
@@ -6,6 +6,10 @@ public class ContingencyTableTest
 {
     public static void main(String[] args)
     {
+        // Tabelle muss in erster Spalte mindestens die maximale Länge haben,
+        // sonst kann es bei der Berechnung zu Fehlern führen.
+        // Vorraussetzung: besser alle Spalten gleich lang und alle Zeilen des Arrays müssen gleich lang sein),
+
         double [][] inTable = {{100, 40, 100}, {60, 50, 20}, {104, 46, 40}};
 
         ContingencyTable a = new ContingencyTable(inTable);
